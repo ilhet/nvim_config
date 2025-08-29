6f3d19765c208d4abb5bad8c2a043d35f9381bc6
Enno Li <0903enno@gmail.com>
2025-08-29 05:29:31 +0200

    This is the initial commit.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'lervag/vimtex',
+  lazy = false,
+  vimtex_complete_close_braces = 1,
+  init = function()
+    vim.g.vimtex_view_method = 'zathura'
+  end,
+}
