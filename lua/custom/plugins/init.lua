f225a647bb0d615e7ae790961007b916c470fb19
Enno Li <0903enno@gmail.com>
2025-08-29 06:12:46 +0200

    Added texlab-lsp.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.lsp.enable 'texlab'
 return {
   'lervag/vimtex',
   lazy = false,
